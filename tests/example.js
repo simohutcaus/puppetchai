6060995eb4f10e57f0664405bc4e264c2c882336
simohutcaus <chocy23@hotmail.com>
2019-09-16 15:42:13 +1000

    second

--- tests/example.js
+++ tests/example.js
@@ -1,5 +1,14 @@
 const puppeteer = require('puppeteer');
 const expect = require('chai').expect;
+const config = require('../lib/config');
+const click = require('../lib/helpers').click
+const type = require('../lib/helpers').typeText
+const loadUrl = require('../lib/helpers').loadUrl
+const getText = require('../lib/helpers').getText
+const waitForText = require('../lib/helpers').waitForText
+const pressKey = require('../lib/helpers').pressKey
+const shouldExist = require('../lib/helpers').shouldExist
+const shouldNotExist = require('../lib/helpers').shouldNotExist
 
 describe('My first pupeteer test', () => {
   let browser
@@ -7,15 +16,16 @@ describe('My first pupeteer test', () => {
 
   before(async function () {
     browser = await puppeteer.launch({
-      headless: true,
-      slowMo: 0,
-      devTools: false,
-      timeout: 10000
+      headless: config.headless,
+      slowMo: config.slowMo,
+      devTools: config.isDevTools,
+      timeout: config.timeout
     })
     page = await browser.newPage()
+    await page.setDefaultTimeout(config.waitingTimeout)
     await page.setViewport({
-      width: 800,
-      height: 600
+      width: config.viewportWidth,
+      height: config.viewportHeight
     })
   })
 
@@ -26,8 +36,13 @@ describe('My first pupeteer test', () => {
 
 
   it('my first test step', async () => {
-    await page.goto("https://dev.to")
-    await page.waitForSelector('#nav-search')
+    
+    await loadUrl(page, config.baseUrl)
+    //await page.goto(config.baseUrl)
+    //await page.waitForSelector('#nav-search')
+    await shouldExist(page, '#nav-search')
+
+    await waitForText(page, 'body', 'WRITE A POST')
 
     const url = await page.url()
     const title = await page.title()
@@ -38,7 +53,9 @@ describe('My first pupeteer test', () => {
 
   it('browser reload', async () => {
     await page.reload()
-    await page.waitForSelector('#page-content')
+    //await page.waitForSelector('#page-content')
+    await shouldExist(page, '#page-content')
+
     const url = await page.url()
     const title = await page.title()
 
@@ -48,9 +65,25 @@ describe('My first pupeteer test', () => {
   })
 
   it('click method', async () => {
-    await page.goto('https://dev.to')
-    await page.waitForSelector('#write-link')
-    await page.click('#write-link')
-    await page.waitForSelector('.registration-rainbow')
+    await loadUrl(page, config.baseUrl)
+    //await page.waitForSelector('#write-link')
+    //await page.click('#write-link')
+    await click(page, "#write-link")
+    //await page.waitForSelector('.registration-rainbow')
+    await shouldExist(page, '.registration-rainbow')
+
   })
+
+  it('submit searchbox', async () => {
+
+    await loadUrl(page, config.baseUrl)
+    await type(page, "javascript", "#nav-search")
+    //await page.waitForSelector('#nav-search')
+    //await page.type("#nav-search", "javascript")
+    //await page.keyboard.press("Enter")
+    await pressKey(page, 'Enter')
+    //await page.waitForSelector('#articles-list')
+    await shouldExist(page, '#articles-list')
+  })
+
 })
